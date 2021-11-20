421e3f3b8bf58fb6556f98c1794669943b1551b7
yeferson <yefersonvelandia4@gmail.com>
2021-11-20 12:02:20 -0500

    App de paises construida con Angular

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interfaces';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+  termino : string  = '';
+  hayError: boolean = false;
+  paises  : Country[] = [];
+
+  constructor( private paisService: PaisService) { 
+  }
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino)
+
+    this.paisService.buscarPais( this.termino )
+        .subscribe( ( paises ) => {
+
+          console.log( paises );          
+          this.paises = paises;
+
+        }, (err)=> {
+
+          this.paises = [];
+          this.hayError = true;
+        })
+  }
+  
+
+}
